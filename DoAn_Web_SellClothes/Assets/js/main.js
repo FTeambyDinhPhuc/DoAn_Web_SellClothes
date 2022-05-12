10633f4dd8000507cafdbb4053565ea2d309e494
Đình Phúc <38375962+dinhphuc1309@users.noreply.github.com>
2022-05-12 16:33:49 +0700

    Tạo cấu trúc web, chia phân vùng custumer và admin, tạo màn hình chính.

---
+++ DoAn_Web_SellClothes/Assets/js/main.js
@@ -0,0 +1,7 @@
+﻿//hiệu ứng Active các button trên nav
+const list = document.querySelectorAll('.navbar__item .navbar__item-link');
+function activeLink() {
+    list.forEach((item) => item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) => item.addEventListener('click', activeLink));
