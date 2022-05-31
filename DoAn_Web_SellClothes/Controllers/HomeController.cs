e85314bb9d526211f850f0262626311ec18f3fa7
Lương Linh <58936434+maluonglinh2001@users.noreply.github.com>
2022-05-31 20:21:30 +0700

    UpdateProduct-ProductDetails

--- DoAn_Web_SellClothes/Controllers/HomeController.cs
+++ DoAn_Web_SellClothes/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace DoAn_Web_SellClothes.Controllers
 {
     public class HomeController : Controller
--- DoAn_Web_SellClothes/Controllers/ProductController.cs
+++ DoAn_Web_SellClothes/Controllers/ProductController.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DoAn_Web_SellClothes.Models;
+using PagedList;
 namespace DoAn_Web_SellClothes.Controllers
 {
     public class ProductController : Controller
     {
         DataClasses1DataContext data = new DataClasses1DataContext();
         // GET: Product
+        private List<Product> LaySanPhamMoi(int count)
+        {
+            return data.Products.OrderByDescending(s => s.IdProduct).Take(count).ToList();
+        }
+
         public ActionResult ProductPage()
         {
             return View();
         }
-        private List<Product> LaySanPhamMoi(int count)
-        {
-            return data.Products.OrderByDescending(s => s.UpdateDate).Take(count).ToList();
-        }
+
         public ActionResult SanPhamMoi()
         {
             var sanphammoi = LaySanPhamMoi(4);
@@ -33,10 +36,28 @@ namespace DoAn_Web_SellClothes.Controllers
             var sanphamnu = from spnu in data.ProductTypes where spnu.IdSex == 0 select spnu;
             return PartialView(sanphamnu);
         }
-        public ActionResult AllSanPham()
+        public ActionResult AllSanPham(int? page)
         {
-            var allsanpham = from allsp in data.Products select allsp;
-            return PartialView(allsanpham);
+            // 1. Tham số int? dùng để thể hiện null và kiểu int
+            // page có thể có giá trị là null và kiểu int.
+
+            // 2. Nếu page = null thì đặt lại là 1.
+            if (page == null) page = 1;
+
+            // 3. Tạo truy vấn, lưu ý phải sắp xếp theo trường nào đó, ví dụ OrderBy
+            // theo LinkID mới có thể phân trang.
+            var allsanpham = (from allsp in data.Products select allsp).ToList();
+
+            // 4. Tạo kích thước trang (pageSize) hay là số Link hiển thị trên 1 trang
+            int pageSize = 9;
+
+            // 4.1 Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
+            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
+            int pageNumber = (page ?? 1);
+
+            // 5. Trả về các Link được phân trang theo kích thước và số trang.
+            allsanpham = allsanpham.OrderByDescending(p => p.IdProduct).ToList();
+            return PartialView(allsanpham.ToPagedList(pageNumber,pageSize));
         }
         public ActionResult SPTheoLoaiNam(int id)
         {
