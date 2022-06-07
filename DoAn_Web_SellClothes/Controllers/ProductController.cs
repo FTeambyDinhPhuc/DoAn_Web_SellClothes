57ec7284eb678ebf63e11fb6bda348326c055aac
Lương Linh <58936434+maluonglinh2001@users.noreply.github.com>
2022-06-07 11:49:38 +0700

    search

--- DoAn_Web_SellClothes/Controllers/ProductController.cs
+++ DoAn_Web_SellClothes/Controllers/ProductController.cs
@@ -16,11 +16,6 @@ namespace DoAn_Web_SellClothes.Controllers
             return data.Products.OrderByDescending(s => s.IdProduct).Take(count).ToList();
         }
 
-        public ActionResult ProductPage()
-        {
-            return View();
-        }
-
         public ActionResult SanPhamMoi()
         {
             var sanphammoi = LaySanPhamMoi(4);
@@ -36,38 +31,20 @@ namespace DoAn_Web_SellClothes.Controllers
             var sanphamnu = from spnu in data.ProductTypes where spnu.IdSex == 0 select spnu;
             return PartialView(sanphamnu);
         }
-        public ActionResult AllSanPham(int? page)
-        {
-            // 1. Tham số int? dùng để thể hiện null và kiểu int
-            // page có thể có giá trị là null và kiểu int.
 
-            // 2. Nếu page = null thì đặt lại là 1.
-            if (page == null) page = 1;
-
-            // 3. Tạo truy vấn, lưu ý phải sắp xếp theo trường nào đó, ví dụ OrderBy
-            // theo LinkID mới có thể phân trang.
-            var allsanpham = (from allsp in data.Products select allsp).ToList();
-
-            // 4. Tạo kích thước trang (pageSize) hay là số Link hiển thị trên 1 trang
-            int pageSize = 9;
-
-            // 4.1 Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
-            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
-            int pageNumber = (page ?? 1);
-
-            // 5. Trả về các Link được phân trang theo kích thước và số trang.
-            allsanpham = allsanpham.OrderByDescending(p => p.IdProduct).ToList();
-            return PartialView(allsanpham.ToPagedList(pageNumber,pageSize));
+        public ActionResult SPTheoLoaiNam(int? id,int?page)
+        {          
+            var sanphamloainam = (from spln in data.Products where spln.IdProductType == id select spln).OrderByDescending(p=>p.IdProduct);
+            int pageSize = 9; // mỗi trang 8 sản phẩm
+            int pageNum = (page ?? 1); // nếu page = null => pageNum = 1
+            return View(sanphamloainam.ToPagedList(pageNum,pageSize));
         }
-        public ActionResult SPTheoLoaiNam(int id)
+        public ActionResult SPTheoLoaiNu(int?id, int?page)
         {
-            var sanphamloainam = from spln in data.Products where spln.IdProductType == id select spln;
-            return View(sanphamloainam);
-        }
-        public ActionResult SPTheoLoaiNu(int id)
-        {
-            var sanphamloainam = from spln in data.Products where spln.IdProductType == id select spln;
-            return View(sanphamloainam);
+            var sanphamloainu = (from spln in data.Products where spln.IdProductType == id select spln).OrderByDescending(p=>p.IdProduct);
+            int pageSize = 9; // mỗi trang 8 sản phẩm
+            int pageNum = (page ?? 1); // nếu page = null => pageNum = 1
+            return View(sanphamloainu.ToPagedList(pageNum,pageSize));
         }
         public ActionResult ProductDetails(int id)
         {
@@ -79,14 +56,20 @@ namespace DoAn_Web_SellClothes.Controllers
             var sizeproduct = from sp in data.Products join s in data.SizeProducts on sp.IdProduct equals s.IdProduct where sp.IdProduct == id select s.NameSizeProduct;
             return PartialView(sizeproduct);
         }
-        public ActionResult SearchProduct()
+        public ActionResult ProductPage(int?page)
         {
-            String keyword = Request.QueryString["keywordsearch"];
-            if (keyword!=null)
+            string keyword = Request.QueryString["keyword"];
+            Session["keysearch"] = keyword;
+            int pageSize = 9; // mỗi trang 9 sản phẩm
+            int pageNum = (page ?? 1); // nếu page = null => pageNum = 1
+            var listProduct = from sp in data.Products.OrderByDescending(p => p.IdProduct) select sp;
+            if (!String.IsNullOrEmpty(keyword))
             {
-               var listProduct = (from sp in data.Products select sp.NameProduct).ToList();
-               return View(listProduct);
+                var listProduct1 = listProduct.Where(p => p.NameProduct.Contains(keyword));
+                return PartialView(listProduct1.ToPagedList(1,9999));
             }
+            else
+            return PartialView(listProduct.ToPagedList(pageNum,pageSize));
         }
     }
 }
---
+++ DoAn_Web_SellClothes/Models/Giohang.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DoAn_Web_SellClothes.Models;
+namespace DoAn_Web_SellClothes.Models
+{
+    public class Giohang
+    {
+        DataClasses1DataContext data = new DataClasses1DataContext();
+        public int iIdProduct { get; set; }
+        public string iImageProduct { get; set; }
+        public string iNameProduct { get; set; }
+        public int iPriceProduct { get; set; }
+        public int iQuantityProduct { get; set; }
+        public int iTransportFee
+        {
+            get { return 25000; }
+        }
+        public int iThanhTien
+        {
+            get { return iQuantityProduct * iPriceProduct+iTransportFee; }
+        }
+        //Khởi tạo giỏ hành theo Mã sản phẩm truyền vào với số lượng mạc định là 1
+        public Giohang(int idProduct)
+        {
+            iIdProduct = idProduct;
+            Product product = data.Products.Single(n => n.IdProduct == iIdProduct);
+            iImageProduct = product.ImageProduct;
+            iNameProduct = product.NameProduct;
+            iPriceProduct = product.PriceProduct;
+            iQuantityProduct = 1;
+        }
+
+    }
+}
